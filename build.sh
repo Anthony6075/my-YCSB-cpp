b3dead69689da915faa86c2a8b68ea40ca92509d
anthony_in_server <Anthony6075@163.com>
2024-03-17 15:03:05 +0800

    update remote build.sh

--- build.sh
+++ build.sh
@@ -9,10 +9,15 @@ if [[ $LOCAL_OR_REMOTE == 'local' ]] ; then
     thirdparty_libs=thirdparty/local_libs
 else
     thirdparty_libs=thirdparty/remote_libs
-    apt install libgflags-dev
-    apt install libzstd-dev
-    apt install libsnappy-dev
-    apt install swig
+    git config --global http.version HTTP/1.1
+    git config --global user.name anthony_in_server
+    git config --global user.email Anthony6075@163.com
+    apt update
+    apt install -y cmake
+    apt install -y libgflags-dev
+    apt install -y libzstd-dev
+    apt install -y libsnappy-dev
+    apt install -y swig
 fi
 
 cd $thirdparty_libs
